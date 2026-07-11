8f84c8130bd11bf2a341b2a5e2f28a241492aedc
Benjamin Abernathy <ben@babernathy.net>
2026-07-11 08:02:51 -0600

    Updated for 0.3.0 release

--- Formula/toad.rb
+++ Formula/toad.rb
@@ -2,27 +2,27 @@ class Toad < Formula
   desc "A developer-friendly REST client"
   homepage "https://github.com/benabernathy/toad-cli"
   license "MIT"
-  version "0.1.0"
+  version "0.3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/benabernathy/toad-cli/releases/download/v0.1.0/toad-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "1cc30e3638fdc59c8fa323a930fb87ce439333280a13c1a136f1e4b21aab26e3"
+      url "https://github.com/benabernathy/toad-cli/releases/download/v0.3.0/toad-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "20c1182be962e25e75f3c9412d59c658df962a1b0d9222f6ecd737e89c26a117"
     end
     on_intel do
-      url "https://github.com/benabernathy/toad-cli/releases/download/v0.1.0/toad-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "a0e42f957f5f03256f117e8bea74566da2b9f81d28a2b00bbf1bcdc82c93d945"
+      url "https://github.com/benabernathy/toad-cli/releases/download/v0.3.0/toad-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "0c840070f59cfb3b58e9fa76ebb1fa6b1e0a73faed5489cccdb4a07f9165dd08"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/benabernathy/toad-cli/releases/download/v0.1.0/toad-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "bfcfd16c5b9831b5473fb36d874fec07ef608b5260019f6f5f711e22cbc31776"
+      url "https://github.com/benabernathy/toad-cli/releases/download/v0.3.0/toad-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "74c68fbd070903475a0d92449c4a231200540ac0cfef2e1736c6154b0d2a7a31"
     end
     on_intel do
-      url "https://github.com/benabernathy/toad-cli/releases/download/v0.1.0/toad-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2f066e0885cfbb6c4906b41305b0e3e2be3a3d79b637048b4930bb5afcd93437"
+      url "https://github.com/benabernathy/toad-cli/releases/download/v0.3.0/toad-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "962f17732a8dfe8915225feca970f61fd2fba05795ca0992b3422e2c312e00e7"
     end
   end
 
